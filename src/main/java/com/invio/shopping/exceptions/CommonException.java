7d710b75428e70072d187d3116bdda1ee266d130
Mgokayay <aymustafagokay@gmail.com>
2024-04-26 18:52:49 +0300

    common exception created

---
+++ src/main/java/com/invio/shopping/exceptions/CommonException.java
@@ -0,0 +1,14 @@
+package com.invio.shopping.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CommonException extends RuntimeException{
+
+    private HttpStatus httpStatus;
+    public CommonException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
