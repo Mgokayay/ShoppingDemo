e658c3bde3575f11ef08861a16adf294cbca9f17
Mgokayay <aymustafagokay@gmail.com>
2024-04-26 18:51:36 +0300

    address entity created

---
+++ src/main/java/com/invio/shopping/entity/Address.java
@@ -0,0 +1,32 @@
+package com.invio.shopping.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "address",schema = "shopping")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
