6b22e1f5be22eb393128a7e9afa1dc0dff808d4e
Mgokayay <aymustafagokay@gmail.com>
2024-04-26 18:50:02 +0300

    user entity created

---
+++ src/main/java/com/invio/shopping/entity/User.java
@@ -0,0 +1,40 @@
+package com.invio.shopping.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user",schema = "shopping")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "user")
+    @JsonBackReference
+    private List<Address> addressList = new ArrayList<>();
+
+
+}
