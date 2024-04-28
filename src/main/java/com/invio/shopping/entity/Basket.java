e632e9006daa7131665ad6fdc7f86fea6b990e3e
Mgokayay <aymustafagokay@gmail.com>
2024-04-28 21:14:23 +0300

    basket entity created

---
+++ src/main/java/com/invio/shopping/entity/Basket.java
@@ -0,0 +1,26 @@
+package com.invio.shopping.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "basket",schema = "shopping")
+public class Basket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonBackReference
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "basket")
+    private List<Product> productList = new ArrayList<>();
+}
