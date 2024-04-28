798cf9630a62bdc7b0f992eb5de124e436c808ab
Mgokayay <aymustafagokay@gmail.com>
2024-04-28 21:14:09 +0300

    adress service implementation edited

--- src/main/java/com/invio/shopping/service/AddressServiceImpl.java
+++ src/main/java/com/invio/shopping/service/AddressServiceImpl.java
@@ -1,8 +1,10 @@
 package com.invio.shopping.service;
 
+import com.invio.shopping.dto.AddressResponse;
 import com.invio.shopping.entity.Address;
 import com.invio.shopping.exceptions.CommonException;
 import com.invio.shopping.repository.AddressRepository;
+import com.invio.shopping.util.AddressDtoConvertion;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,37 +18,39 @@ public class AddressServiceImpl implements AddressService{
 
     private final AddressRepository addressRepository;
     @Override
-    public Address save(Address address) {
-        return addressRepository.save(address);
+    public AddressResponse save(Address address) {
+        addressRepository.save(address);
+        return AddressDtoConvertion.convertAddress(address);
     }
 
     @Override
-    public List<Address> findAll() {
-        return addressRepository.findAll();
+    public List<AddressResponse> findAll() {
+
+        return AddressDtoConvertion.convertAddressList(addressRepository.findAll());
     }
 
     @Override
-    public Address findById(Long id) {
+    public AddressResponse findById(Long id) {
         Optional<Address> addressOptional = addressRepository.findById(id);
         if(addressOptional.isPresent()){
-            return addressOptional.get();
+            return AddressDtoConvertion.convertAddress(addressOptional.get());
         }
         throw new CommonException("Addres is not exist with given id " + id, HttpStatus.NOT_FOUND);
 
     }
 
     @Override
-    public Address delete(Long id) {
+    public AddressResponse delete(Long id) {
         Optional<Address> addressOptional = addressRepository.findById(id);
         if(addressOptional.isPresent()){
             addressRepository.delete(addressOptional.get());
-            return addressOptional.get();
+            return AddressDtoConvertion.convertAddress(addressOptional.get());
         }
         throw new CommonException("Addres is not exist with given id " + id, HttpStatus.NOT_FOUND);
     }
 
     @Override
-    public Address update(Long id, Address address) {
+    public AddressResponse update(Long id, Address address) {
         Optional<Address> addressOptional = addressRepository.findById(id);
         if(addressOptional.isPresent()){
             Address existingAddress = addressOptional.get();
@@ -54,7 +58,7 @@ public class AddressServiceImpl implements AddressService{
             existingAddress.setCity(address.getCity());
             existingAddress.setCountry(address.getCountry());
             addressRepository.save(existingAddress);
-            return existingAddress;
+            return AddressDtoConvertion.convertAddress(existingAddress);
         }
         throw new CommonException("Addres is not exist with given id " + id, HttpStatus.NOT_FOUND);
     }
