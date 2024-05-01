056131883fa166731728fc91f229737141a0aed1
Mgokayay <aymustafagokay@gmail.com>
2024-05-01 19:40:07 +0300

    query added to the user repo

--- src/main/java/com/invio/shopping/repository/UserRepository.java
+++ src/main/java/com/invio/shopping/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.invio.shopping.repository;
 
 import com.invio.shopping.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(String email);
 }
